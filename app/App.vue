477fee898fce858f4f7f83797004ea051ea962c9
Kristjan Pihus <kristjan.pihus@eesti.ee>
2017-02-18 15:17:37 +0200

    Messaging part sort of finished

--- app/App.vue
+++ app/App.vue
@@ -2,28 +2,42 @@
     <div id="app">
         <navbar placement="top" type="default">
             <a slot="brand" href="#" title="Sid Commander" class="navbar-brand">Sid Commander</a>
-            <dropdown text="Dropdown">
-                <li><a href="link">Option</a></li>
+            <dropdown text="Messages">
+                <li class="menu_item"><span><strong>Add new</strong></span></li>
             </dropdown>
         </navbar>
-        <div class="container">
+        <div class="container-fluid">
             <accordion type="info" :one-at-atime="checked">
                 <panel v-for="item in commands">
                 <strong slot="header"><u>{{item.title}}</u></strong>
-                    {{item.message}}
+                    <div class="row">
+                        <div class="col-md-10">
+                            {{item.message}}
+                        </div>
+                        <div class="col-md-2">
+                            <button class="btn btn-success" @click="sendMessage(item.message)">Send</button>
+                        </div>
+                    </div>
+
                 </panel>
             </accordion>
         </div>
     </div>
 </template>
+<style>
+    .menu_item{
+        cursor: pointer;
+        padding: 10px
+    }
+</style>
 
 <script>
-  import {accordion, panel, navbar} from 'vue-strap';
+  import {accordion, panel, navbar, dropdown} from 'vue-strap';
   export default {
 
 
     components: {
-      accordion, panel, navbar
+      accordion, panel, navbar, dropdown
     },
 
     data () {
@@ -36,9 +50,21 @@
 
     created () {
       console.log('CREATED',); //TODO: REMOVE
+      this.getMessages();
     },
 
-    methods: {}
+    methods: {
+      getMessages(){
+        this.$http.get('http://localhost:3000/messages')
+          .then(function (response) {
+            console.log(response)
+            this.$set('commands', response.body)
+          })
+      },
+      sendMessage(text){
+        console.log(text)
+      }
+    }
 
   }
 </script>
--- app/main.js
+++ app/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
+import VueWebsocket from "vue-websocket";
+Vue.use(VueWebsocket, "ws://localhost:3000");
+Vue.use(VueResource);
 
 new Vue({
   el: 'body',
